dacd98e1de520251eeebcbee3fd8136a3f097f8a
Svjatoslav Kachmar <pompodar@gmail.com>
2023-03-27 18:37:14 +0300

    add_personal_firebase_config

--- script.js
+++ script.js
@@ -4,16 +4,6 @@
  "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
  import { getStorage, ref as heck, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.2/firebase-storage.js';
 
- const firebaseConfig = {
- apiKey: "AIzaSyDO4jPM5gRhvtKvPo05hrWlDz7hOirTQww",
- authDomain: "trivia-for-two.firebaseapp.com",
- projectId: "trivia-for-two",
- storageBucket: "trivia-for-two.appspot.com",
- messagingSenderId: "194595752794",
- appId: "1:194595752794:web:c6ea94b7350bfe3f46c163",
- databaseURL: "https://trivia-for-two-default-rtdb.europe-west1.firebasedatabase.app"
-};
-
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const database = getDatabase(app);
