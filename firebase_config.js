ce6142970f627debe5e539e9253e332a61f6daec
Svjatoslav Kachmar <pompodar@gmail.com>
2023-03-27 18:50:58 +0300

    add gitinore

--- firebase_config.js
+++ firebase_config.js
@@ -6,4 +6,6 @@ const firebaseConfig = {
     messagingSenderId: "194595752794",
     appId: "1:194595752794:web:c6ea94b7350bfe3f46c163",
     databaseURL: "https://trivia-for-two-default-rtdb.europe-west1.firebasedatabase.app"
-   };
+};
+
+// hi
